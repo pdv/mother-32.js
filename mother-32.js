71ebf9fbfd252604ed93a5ed7b09f65f20aa0b77
pdv <philipjdelvecchio@gmail.com>
2017-05-26 14:01:08 -0400

    more knobs

--- mother-32.js
+++ mother-32.js
@@ -17,12 +17,27 @@ const gctx = canvas.getContext('2d');
 // Knobs
 
 const knobWidth = 60;
-const knobFrames = 31;
+const knobFrames = 63;
+const knobDragScale = 0.0005;
 const knobImg = new Image();
-knobImg.src = "knob.png";
+const knobImgWidth = 240;
+knobImg.src = "moogknob.png";
+knobImg.onload = draw;
 
+const woodImg = new Image();
+woodImg.src = "wood.jpg";
+
+const leftPadding = 50;
+const topPadding = 30;
+const knobSpace = 100;
 const knobs = {
-    frequency: { label: "frequency", x: 20, y: 20, val: 0, min: -1, max: 1 }
+    frequency: { label: "frequency", x: 0, y: 0, value: 0, min: -1, max: 1, onChange: (val) => {
+    }},
+    pulseWidth: { label: "pulse width", x: 2, y: 0, value: 0.5, min: 0, max: 1, onChange: (val) => {}},
+    glide: { label: "glide", x: 0, y: 1, value: 0, min: 0, max: 1, onChange: (val) => {}},
+    tempo: { label: "tempo/gate", x: 0, y: 2, value: 0, min: 0, max: 1, onChange: (val) => {}},
+    lfoRate: { label: "lfo rate", x: 1, y: 2, value: 0, min: 0, max: 1, onChange: (val) => {}}
+
 };
 
 let mouse = {
@@ -35,6 +50,10 @@ function contains(knob, mousePos) {
         && knob.y < mousePos.y && mousePos.y < knob.y + knobWidth;
 }
 
+function clip(value, min, max) {
+    return Math.max(min, Math.min(value, max));
+}
+
 function getMousePos(canvas, event) {
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
@@ -42,23 +61,23 @@ function getMousePos(canvas, event) {
     return { x: x, y: y };
 }
 
-canvas.addEventListener('mousedown', (e) => {
+document.addEventListener('mousedown', (e) => {
     mouse.down = mouse.current = getMousePos(canvas, e);
 }, false);
 
-canvas.addEventListener('mouseup', (e) => {
+document.addEventListener('mouseup', (e) => {
     mouse.current = getMousePos(canvas, e);
     mouse.down = undefined;
 }, false);
 
-canvas.addEventListener('mousemove', (e) => {
+document.addEventListener('mousemove', (e) => {
     mouse.current = getMousePos(canvas, e);
     if (mouse.down) {
-        let dy = mouse.down.y - mouse.current.y;
+        let dy = (mouse.down.y - mouse.current.y) * knobDragScale;
         for (let knobKey in knobs) {
             let knob = knobs[knobKey];
             if (contains(knob, mouse.down)) {
-                knob.value += dy;
+                knob.value = clip(knob.value + dy, knob.min, knob.max);
                 knob.onChange(knob.value);
                 break;
             }
@@ -66,12 +85,36 @@ canvas.addEventListener('mousemove', (e) => {
     }
 }, false);
 
+function drawBg() {
+    gctx.fillStyle = '#000';
+    gctx.fillRect(0, 0, w, h);
+    gctx.drawImage(woodImg, 0, 0, 20, h);
+    gctx.drawImage(woodImg, w - 20, 0, 20, h);
+}
+
 function draw() {
+    gctx.clearRect(0, 0, w, h);
+    drawBg();
     for (let knobKey in knobs) {
         const knob = knobs[knobKey];
-        
-        gctx.drawImage(knobImg, 0, knob.
+        const normalized = (knob.value - knob.min) / (knob.max - knob.min);
+        const frame = Math.floor(normalized * knobFrames);
+        const x = leftPadding + (knob.x * knobSpace);
+        const y = topPadding + (knob.y * knobSpace);
+        gctx.drawImage(knobImg,
+                       0,
+                       frame * knobImgWidth,
+                       knobImgWidth,
+                       knobImgWidth,
+                       x,
+                       y,
+                       knobWidth,
+                       knobWidth);
+        gctx.fillStyle = '#fff';
+        gctx.font = '10px sans-serif';
+        gctx.fillText(knob.label.toUpperCase(), x, y - 5);
     }
+    window.requestAnimationFrame(draw);
 }
 
 // Audio
@@ -81,5 +124,3 @@ let nodes = {
     vco: actx.createOscillator(),
     lfo: actx.createOscillator()
 };
-
-
